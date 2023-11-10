57256319408e9c935809710ba175a8d47faadac3
YeongJae0114 <yjl8628@naver.com>
2023-11-10 15:51:39 +0900

    :tada: Start project
    start

---
+++ yeongjae/my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Sidebar from './Sidebar';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Sidebar />
+
+  </React.StrictMode>
+);
+
+
